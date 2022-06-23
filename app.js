060e2c58e8505894029c4367cb11ec3e25885d5f
nancycrojas <nanclarisaok@gmail.com>
2022-06-23 09:04:59 -0300

    Variables, datos y operadores - ejercicio 8

---
+++ app.js
@@ -0,0 +1,6 @@
+//Días a segundos
+
+//Crear un programa que pida ingresar una cantidad de días y muestre un mensaje con el resultado de la conversión en segundos.
+
+const dias = prompt ("Por favor ingresa la cantidad de días");
+alert("Conversión en segundos: " + dias *24 *60 *60 + " segundos");
