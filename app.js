ff006946a18a3d35da5bfb4063a58e9d053672f3
nancycrojas <nanclarisaok@gmail.com>
2022-09-29 20:08:08 -0300

    Agregando template strings

--- app.js
+++ app.js
@@ -2,5 +2,8 @@
 
 //Crear un programa que pida ingresar una cantidad de días y muestre un mensaje con el resultado de la conversión en segundos.
 
-const dias = prompt ("Por favor ingresa la cantidad de días");
-alert("Conversión en segundos: " + dias *24 *60 *60 + " segundos");
+const dias = Number(prompt("Por favor ingresa la cantidad de días"));
+//alert("Conversión en segundos: " + dias * 24 * 60 * 60 + " segundos");
+
+//Template strings
+alert(`Conversión en segundos: ${dias * 24 * 60 * 60} segundos.`)
